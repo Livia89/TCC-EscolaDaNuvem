61118438b99d6d9ca0ab251ac95a259f7997f468
Livia89 <liviapriscilla1989@gmail.com>
2023-05-25 15:50:21 +0100

    created interfaces

--- src/share/interfaces.ts
+++ src/share/interfaces.ts
@@ -1,4 +1,51 @@
-export interface sidemenuData {
-    imageUrl:string,
-    socialMidias: []
+export interface SidemenuData {
+  imageUrl: string;
+  socialMidias: [];
 }
+
+export interface PersonalData {
+  name: string;
+  phone: string;
+  email: string;
+  address: string;
+}
+
+export interface Socials {
+  linkedin: string;
+  github: string;
+  instagram: string;
+  stackoverflow: string;
+}
+
+export interface WorkExperience {
+  company: string; 
+  date: string;
+  location: string;
+  
+}
+
+export interface Education {
+  date: string;
+  name: string;
+  location: string;
+  level: string;
+}
+
+export interface UsersData {
+name: string;
+phone: string;
+email: string;
+address: string;
+job: string;
+socials: Socials;
+workExperience: WorkExperience[]; 
+education: Education[];
+skills: string[];
+}
+
+export interface Users {
+  id: string;
+  data: UsersData;
+  active: boolean;
+}
+
