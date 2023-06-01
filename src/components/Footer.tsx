719d0530ee37b34e13324eed9856abeab766e217
Livia89 <liviapriscilla1989@gmail.com>
2023-06-01 16:28:20 +0100

    added footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+export const Footer = () => {
+    return (
+      <>
+        <footer className="bg-gray-800  text-white w-full ">
+          <div className="max-w-screen-xl mx-auto p-4 sm:px-6 lg:px-8">
+            <div className="flex justify-between">
+            <p className="text-sm"><span>☁️ </span>TCC - Escola da Nuvem </p>
+              <p className="text-sm">&copy; 2023 - All Rights Reserved.</p>
+            </div>
+          </div>
+        </footer>
+      </>
+    );
+  };
+  
